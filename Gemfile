8355bb0235545f0b289a4c5572b3b44b39622139
Mame <yuuki.mrm@gmail.com>
2020-02-20 08:09:28 +0000

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '5.1.6'
 gem 'bcrypt', '3.1.12'
+gem 'faker', '1.7.3'
+gem 'will_paginate', '3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
 gem 'bootstrap-sass', '3.3.7'
 # Use Puma as the app server
 gem 'puma', '3.9.1'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  #return true, if giving user is logged in user
+  def current_user?(user)
+    user == current_user
+  end
+  
   #return the current user(if it is)
   def current_user
     if (user_id = session[:user_id])
@@ -39,5 +44,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  #redirect back memoraize url (or default)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  #store url is tried to access
+  def store location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
