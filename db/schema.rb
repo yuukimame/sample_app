b65658f7564df9a7f84cf0ef0645b5ce6bdba087
Mame <yuuki.mrm@gmail.com>
2020-02-21 13:31:21 +0000

    extra

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200209180002) do
+ActiveRecord::Schema.define(version: 20200220131934) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20200209180002) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.string "remember_digest"
   end
 
 end
